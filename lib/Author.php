d1dd570bbed97f92aa6becac4ac2521f01eb80f0
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2012-09-27 01:13:26 +0200

    Update UML schema, add class docstring and getter/setter functions

--- lib/Author.php
+++ lib/Author.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * Author
+ *
+ * @author Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
+ * @copyright GNU GPLv3
+ */
 class Author
 {
   protected $name;
@@ -7,4 +13,88 @@ class Author
   protected $email;
 
   protected $website;
+
+  /**
+   * Constructor
+   */
+  public function __construct()
+  {
+  }
+
+  /***********
+   * Getters
+   ***********/
+
+  /**
+   * Get Name
+   *
+   * @return string
+   */
+  public function getName()
+  {
+    return $this->name;
+  }
+
+  /**
+   * Get Email
+   *
+   * @return string
+   */
+  public function getEmail()
+  {
+    return $this->email;
+  }
+
+  /**
+   * Get Website
+   *
+   * @return string
+   */
+  public function getWebsite()
+  {
+    return $this->website;
+  }
+
+  /**********
+   * Setters
+   **********/
+
+  /**
+   * Set Name
+   *
+   * @param string $name
+   * @return Author
+   */
+  public function setName($name)
+  {
+    $this->name = $name;
+
+    return $this;
+  }
+
+  /**
+   * Set Email
+   *
+   * @param string $email
+   * @return Author
+   */
+  public function setEmail($email)
+  {
+    $this->email = $email;
+
+    return $this;
+  }
+
+  /**
+   * Set Website
+   *
+   * @param string $website
+   * @return Author
+   */
+  public function setWebsite($website)
+  {
+    $this->website = $website;
+
+    return $this;
+  }
 }
--- lib/Project.php
+++ lib/Project.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * Project
+ *
+ * @author Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
+ * @copyright GNU GPLv3
+ */
 class Project
 {
   protected $name;
@@ -11,4 +17,147 @@ class Project
   protected $logo;
 
   protected $tags = array();
+
+  /**
+   * Constructor
+   */
+  public function __construct()
+  {
+  }
+
+  /***********
+   * Getters
+   ***********/
+
+  /**
+   * Get Name
+   *
+   * @return string
+   */
+  public function getName()
+  {
+    return $this->name;
+  }
+
+  /**
+   * Get Summary
+   *
+   * @return string
+   */
+  public function getSummary()
+  {
+    return $this->summary;
+  }
+
+  /**
+   * Get Date
+   *
+   * @return DateTime
+   */
+  public function getDate()
+  {
+    return $this->date;
+  }
+
+  /**
+   * Get Logo
+   *
+   * @return string
+   */
+  public function getLogo()
+  {
+    return $this->logo;
+  }
+
+  /**
+   * Get Tags
+   *
+   * @return mixed
+   */
+  public function getTags()
+  {
+    return $this->tags;
+  }
+
+  /**********
+   * Setters
+   **********/
+
+  /**
+   * Set Name
+   *
+   * @param string $name
+   * @return Project
+   */
+  public function setName($name)
+  {
+    $this->name = $name;
+
+    return $this;
+  }
+
+  /**
+   * Set Summary
+   *
+   * @param string $summary
+   * @return Project
+   */
+  public function setSummary($summary)
+  {
+    $this->summary = $summary;
+
+    return $this;
+  }
+
+  /**
+   * Set Date
+   *
+   * @param DateTime $date
+   * @return Project
+   */
+  public function setDate($date)
+  {
+    $this->date = $date;
+
+    return $this;
+  }
+
+  /**
+   * Set Logo
+   *
+   * @param string $logo
+   * @return Project
+   */
+  public function setLogo($logo)
+  {
+    $this->logo = $logo;
+
+    return $this;
+  }
+
+  /**
+   * Set Tags
+   *
+   * @param mixes $tags
+   * @return Project
+   */
+  public function setTags($tags)
+  {
+    $this->tags = $tags;
+
+    return $this;
+  }
+
+  /**
+   * Add Tag
+   *
+   * @param string $tag
+   * @return Project
+   */
+  public function addTag($tag)
+  {
+    $this->tags[] = $tag;
+
+    return $this;
+  }
 }
