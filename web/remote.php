0841d8e52ac4bcb7a4f3a5c828723ce957b4d2b6
Sofiane Sadi <sofiane.sadi@gmail.com>
2013-02-21 23:41:57 +0100

    Working on remote design.

--- web/remote.php
+++ web/remote.php
@@ -40,12 +40,11 @@
       <div class="span6" id="expo-remote-base-control">
         <h1 class="span12">Name project</h1>
         <hr class="span12"/>
-        <a id="expo-remote-previous"><span>PREVIOUS</span></a>
+        <a id="expo-remote-previous"><span></span></a>
         <a id="expo-remote-current"><span>0</span></a>
-        <a id="expo-remote-next"><span>NEXT</span></a>
+        <a id="expo-remote-next"><span></span></a>
         <input id="expo-remote-name" type="text" placeholder="#0" />
-        <hr class="span12"/>
-        <div id="expo-remote-followers-counter">0</div>
+        <div id="expo-remote-followers-counter" class="span6 offset4"><span></span>0</div>
         <div class="footer visible-phone">
           <a id="show-advanced-control">^</a>
         </div>
