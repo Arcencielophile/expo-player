75acbde8ca9b6ce1671e867ffd42ab9cf0dca857
Vaince <vincent.saisset@gmail.com>
2013-03-07 21:29:34 +0100

    ignore config

--- config/config.inc.php_dev.php
+++ config/config.inc.php_dev.php
@@ -1,6 +1,6 @@
 <?php
-  $expo_srv        = 'http://dev.expo-player.fr';
+  $expo_srv        = 'http://192.168.0.46';
   $expo_data_src   = sprintf('%s/%s', $expo_srv, 'cdn/expo/data/whatisexpo.xml');
   $expo_theme_src  = sprintf('%s/%s', $expo_srv, 'cdn/expo/expo.xml');
-  $expo_remote_srv = 'http://127.0.0.1:2890';
+  $expo_remote_srv = 'http://192.168.0.46:2890';
 ?>
