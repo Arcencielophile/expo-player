bdbc3377eb673886b2f475be4b46c99713cb950f
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2013-03-07 22:08:20 +0100

    USe the new config (simplify)

--- config/config.inc.php_dev.php
+++ config/config.inc.php_dev.php
@@ -1,6 +1,5 @@
 <?php
   $expo_srv        = 'http://127.0.0.1';
-  $expo_data_src   = sprintf('%s/%s', $expo_srv, 'cdn/expo/data/whatisexpo.xml');
-  $expo_theme_src  = sprintf('%s/%s', $expo_srv, 'cdn/expo/expo.xml');
+  $expo_data_src   = 'http://127.0.0.1/cdn/expo/expo.xml';
   $expo_remote_srv = 'http://127.0.0.1:2890';
 ?>
